316d3b6cf80b4da31cce1d3a7eee5f60cb5d1e23
JorgeGB-Creator <jorgegarcia32145@gmail.com>
2025-03-21 23:26:47 -0400

    cree un generador de contraseña  :)

---
+++ passwords.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+
+def generar_contraseña(tamaño=12, mayusculas=True, numeros=True, especiales=True):
+    caracteres = string.ascii_lowercase
+    if mayusculas:
+        caracteres += string.ascii_uppercase
+    if numeros:
+        caracteres += string.digits
+    if especiales:
+        caracteres += string.punctuation
+
+    contraseña_generada = ''.join(random.choice(caracteres)
+                                  for _ in range(tamaño))
+    return contraseña_generada
+
+
+if __name__ == "__main__":
+    longitud = int(input("Longitud de la contraseña: "))
+    incluir_mayus = input("¿Incluir mayúsculas? (s/n): ").lower() == 's'
+    incluir_numeros = input("¿Incluir números? (s/n): ").lower() == 's'
+    incluir_especiales = input("¿Incluir especíales? (s/n): ").lower() == 's'
+
+    contraseña_final = generar_contraseña(
+        longitud, incluir_mayus, incluir_numeros, incluir_especiales)
+    print(f"Contraseña generada: {contraseña_final}")
